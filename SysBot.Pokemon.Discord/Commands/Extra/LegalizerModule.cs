699ff05e7460ce38c2aa97848f57c37f888d8538
Gengar <80122551+bdawg1989@users.noreply.github.com>
2024-08-22 12:55:53 -0500

    Delete users convert/legalize command request to clean channel.

--- SysBot.Pokemon.Discord/Commands/Extra/LegalizerModule.cs
+++ SysBot.Pokemon.Discord/Commands/Extra/LegalizerModule.cs
@@ -1,5 +1,7 @@
+using Discord;
 using Discord.Commands;
 using PKHeX.Core;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SysBot.Pokemon.Discord
@@ -9,27 +11,39 @@ namespace SysBot.Pokemon.Discord
         [Command("convert"), Alias("showdown")]
         [Summary("Tries to convert the Showdown Set to RegenTemplate format.")]
         [Priority(1)]
-        public Task ConvertShowdown([Summary("Generation/Format")] byte gen, [Remainder][Summary("Showdown Set")] string content)
+        public async Task ConvertShowdown([Summary("Generation/Format")] byte gen, [Remainder][Summary("Showdown Set")] string content)
         {
-            return Task.Run(() => Context.Channel.ReplyWithLegalizedSetAsync(content, gen));
+            var deleteMessageTask = DeleteCommandMessageAsync(Context.Message, 2000);
+            var convertTask = Task.Run(() => Context.Channel.ReplyWithLegalizedSetAsync(content, gen));
+            await Task.WhenAll(deleteMessageTask, convertTask).ConfigureAwait(false);
         }
 
         [Command("convert"), Alias("showdown")]
         [Summary("Tries to convert the Showdown Set to RegenTemplate format.")]
         [Priority(0)]
-        public Task ConvertShowdown([Remainder][Summary("Showdown Set")] string content)
+        public async Task ConvertShowdown([Remainder][Summary("Showdown Set")] string content)
         {
-            return Task.Run(() => Context.Channel.ReplyWithLegalizedSetAsync<T>(content));
+            var deleteMessageTask = DeleteCommandMessageAsync(Context.Message, 2000);
+            var convertTask = Task.Run(() => Context.Channel.ReplyWithLegalizedSetAsync<T>(content));
+            await Task.WhenAll(deleteMessageTask, convertTask).ConfigureAwait(false);
         }
 
         [Command("legalize"), Alias("alm")]
         [Summary("Tries to legalize the attached pkm data and output as RegenTemplate.")]
         public async Task LegalizeAsync()
         {
-            foreach (var att in Context.Message.Attachments)
-            {
-                await Task.Run(() => Context.Channel.ReplyWithLegalizedSetAsync(att)).ConfigureAwait(false);
-            }
+            var deleteMessageTask = DeleteCommandMessageAsync(Context.Message, 2000);
+            var legalizationTasks = Context.Message.Attachments.Select(att =>
+                Task.Run(() => Context.Channel.ReplyWithLegalizedSetAsync(att))
+            ).ToArray();
+
+            await Task.WhenAll(deleteMessageTask, Task.WhenAll(legalizationTasks)).ConfigureAwait(false);
+        }
+
+        private async Task DeleteCommandMessageAsync(IUserMessage message, int delayMilliseconds)
+        {
+            await Task.Delay(delayMilliseconds).ConfigureAwait(false);
+            await message.DeleteAsync().ConfigureAwait(false);
         }
     }
 }
