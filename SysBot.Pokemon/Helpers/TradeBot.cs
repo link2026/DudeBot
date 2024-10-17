db2d501e950e8cc5ee6f0de093b990fe44fee87e
Gengar <80122551+bdawg1989@users.noreply.github.com>
2024-10-16 19:18:54 -0500

    Update dependencies - Support Shiny Meloetta.wc9 - Support Birthday Tandemaus

--- SysBot.Pokemon/Helpers/TradeBot.cs
+++ SysBot.Pokemon/Helpers/TradeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v3.2";
+        public const string Version = "v3.3";
     }
 }
