72bf19f9f7bb9d41751073e23b2e7f3cc1fa2fe1
Gengar <80122551+bdawg1989@users.noreply.github.com>
2024-12-10 10:49:50 -0600

    Fixes Keldeo, Deoxys, Zarude start date

--- SysBot.Pokemon/Helpers/TradeBot.cs
+++ SysBot.Pokemon/Helpers/TradeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v4.5";
+        public const string Version = "v4.6";
     }
 }
